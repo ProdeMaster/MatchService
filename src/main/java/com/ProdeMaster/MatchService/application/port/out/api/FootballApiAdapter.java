12220d405fc0fe58d8e9cdc9d4e0940e4d82be0e
Gaston Herrlein <herrleingaston@gmail.com>
2026-01-12 03:21:34 +0100

    fix(adapter): update the FootballApiAdapter to remove the constants since they are already defined.

--- src/main/java/com/ProdeMaster/MatchService/application/port/out/api/FootballApiAdapter.java
+++ src/main/java/com/ProdeMaster/MatchService/application/port/out/api/FootballApiAdapter.java
@@ -3,14 +3,11 @@ package com.ProdeMaster.MatchService.application.port.out.api;
 import com.ProdeMaster.MatchService.application.dto.MatchDto;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public interface FootballApiAdapter {
 
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    DateTimeFormatter formatterToDataTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    List<MatchDto> getWeeklyMatches();
 
-    List<MatchDto> getWeeklyMatches ();
-    LocalDateTime parseToLocalDateTime (String date);
+    LocalDateTime parseToLocalDateTime(String date);
 }
